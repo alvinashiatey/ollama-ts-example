cf4fe4e212f412cb8bd01944bf2fb1461c6449a6
Alvin Ashiatey <alvinashiatey@gmail.com>
2024-02-29 10:08:04 -0500

    push

---
+++ dist/app.js
@@ -0,0 +1,23 @@
+import ollama from "ollama";
+const args = process.argv.slice(2);
+const schema = {
+    colors: [
+        {
+            name: "string",
+            hex: "string",
+        },
+    ],
+};
+const response = await ollama.chat({
+    model: "tinyllama",
+    messages: [
+        {
+            role: "system",
+            content: `You are a helpful AI assistant. The user will request colors and you the assistant will return 5 colors in hex format. Output in JSON using the schema: ${JSON.stringify(schema)}.`,
+        },
+        { role: "user", content: `What colors are best for ${args}` },
+    ],
+    format: "json",
+});
+console.log(response.message.content);
+//# sourceMappingURL=app.js.map
